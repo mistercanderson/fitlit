066e52158b4f3b359c3c238edb803058669fb77f
Melanie Daoheuang <daoh0031@gmail.com>
2021-03-30 16:04:29 -0600

    Change data to userList

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,19 +1,25 @@
 class UserRepository {
-  constructor(data) {
-    this.data = data;
+  constructor(users) {
+    this.userList = users;
   }
 
   returnUserData(userID) {
-    const user = this.data.find(user => user.id === userID);
+    const user = this.userList.find(user => user.id === userID);
     return user;
   }
 
   allUsersAverageSteps() {
-    let sum = this.data.reduce((acc, user) => {
+    let sum = this.userList.reduce((acc, user) => {
       return acc + user.dailyStepGoal;
   }, 0);
-  return sum / this.data.length;
+  return sum / this.userList.length;
+  }
+
+  convertUserList() {
+    this.userList.map((user) => {
+      user = new User;
+    })
   }
 }
 
-module.export = UserRepository;
+module.exports = UserRepository;
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,59 +1,41 @@
 const chai = require('chai');
 const expect = chai.expect;
 const UserRepository = require('../src/UserRepository');
-// const User = require('../src/User');
+
 
 describe('UserRepository', () => {
-  // let userRepo;
-  // const fakeData = [
-  //   {
-  //     "id": 1,
-  //     "name": "Shay Mitchel",
-  //     "address": "809 Cherry Lane Trail, Minneapolis MN 55125",
-  //     "email": "bad.and.boujee@gmail.com",
-  //     "strideLength": 4.5,
-  //     "dailyStepGoal": 10000,
-  //     "friends": [19, 9, 31]
-  //   }
-  // ];
-  beforeEach((done) => {
-
-    let userRepo;
-    const fakeData = [
-      {
-        "id": 1,
-        "name": "Shay Mitchel",
-        "address": "809 Cherry Lane Trail, Minneapolis MN 55125",
-        "email": "bad.and.boujee@gmail.com",
-        "strideLength": 4.5,
-        "dailyStepGoal": 10000,
-        "friends": [19, 9, 31]
-      }
-    ];
+  let userRepo;
+  const fakeData = [
+    {
+      "id": 1,
+      "name": "Shay Mitchel",
+      "address": "809 Cherry Lane Trail, Minneapolis MN 55125",
+      "email": "bad.and.boujee@gmail.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 10000,
+      "friends": [19, 9, 31]
+    }
+  ];
+  
+  beforeEach(() => {
     userRepo = new UserRepository(fakeData);
-    
-    // done();
-    return console.log('made it')
-    .end(done)
   });
- 
-  // const user1 = new User(1, "Shay Mitchel", "809 Cherry Lane Trail, Minneapolis MN 55125", "shay.mitchel@gmail.com", 4.5, 10000, [19, 9, 31]);
 
   it('should be an instance of UserRepository', () => {
     expect(userRepo).to.be.an.instanceOf(UserRepository);
   });
 
-  it.skip('should track a list of User instances', () => {
-    expect(userRepo.data).to.be.equal(fakeData);
+  it('should track a list of User instances', () => {
+    expect(userRepo.userList).to.be.equal(fakeData);
   }); 
   
-  it.skip('should return a user\'s data based off of their ID', () => {
-    user1.returnUserData(1);
+  it('should return a user\'s data based off of their ID', () => {
+    userRepo.returnUserData(1);
     expect(userRepo.returnUserData(1)).to.be.equal(fakeData[0]);
   });
 
-  it.skip('should return the average step goal amogst all users', () => {
-    user1.allUsersAverageSteps();
+  it('should return the average step goal amogst all users', () => {
+    userRepo.allUsersAverageSteps();
     expect(userRepo.allUsersAverageSteps()).to.be.equal(10000);
   }); 
 });
