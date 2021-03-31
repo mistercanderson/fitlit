e0c45edbfa0795ec393a40c4d1eb093a631fae92
Melanie Daoheuang <daoh0031@gmail.com>
2021-03-30 22:46:02 -0600

    Add Hydration properties and method for ounces that day

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,3 +1,19 @@
+class Hydration {
+  constructor(userID, data) {
+    this.userID = userID;
+    this.filterData = data.filter(element => element.userID === userID);
+  }
+
+  ouncesThatDay(date) {
+    let thisDay;
+ 
+    thisDay = this.filterData.filter(element => element.date === date);
+    return thisDay[0].numOunces;
+  }
+}
+
+
+
 if (typeof module !== 'undefined') {
   module.exports = Hydration;
 }
--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -2,27 +2,25 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Hydration = require('../src/Hydration');
-const fakeData2 = ('..data/fakeData2')
+const fakeData2 = require('../data/fakeData2');
 
 describe('Hydration', () => {
-  const hydration = new Hydration();
 
-  it.skip('should be an instance of Hydration', () => {
+  const hydration = new Hydration(1, fakeData2);
+
+  it('should be an instance of Hydration', () => {
     expect(hydration).to.be.an.instanceOf(Hydration);
   });
 
   it.skip('should return the average fluid ounces consumed for all time', () => {
-    hydration.totalOunces();
     expect(hydration.totalOunces()).to.be.equal(495);
   });
 
-  it.skip('should know how many fluid ounces was consumed for a specific day', () => {
-    hydration.ouncesToday(date);
-    expect(hydration.ouncesToday()).to.be.equal(95)
+  it('should know how many fluid ounces was consumed for a specific day', () => {
+    expect(hydration.ouncesThatDay("2019/06/15")).to.be.equal(95);
   });
 
   it.skip('should return how many fluid ounces consumed each day over a week', () => {
-    hydration.weeklyHydration();
     expect(hydration.weeklyHydration()).to.be.equal(495);
   });
 });
