624b06a08ff23cff3741db0e26247c35791f918f
Melanie Daoheuang <daoh0031@gmail.com>
2021-03-30 20:28:26 -0600

    Display name in welcome message

--- src/scripts.js
+++ src/scripts.js
@@ -9,8 +9,24 @@ const userGreeting = document.querySelector('.user-greeting');
 
 const testUsers = new UserRepository(userData);
 
-const testUser = new User(testUsers.userList[0]);
+// const testUser = new User(testUsers.userList[0]);
 
+
+
+window.addEventListener('load', displayWelcome);
+
+function getRandomNum (array) {
+  return Math.floor(Math.random() * array.length)
+};
+
+function displayWelcome() {
+  let randomUser = userData[getRandomNum(userData)];
+  let user = new User(randomUser);
+  
+  userGreeting.innerHTML += `
+  <h1>Let's Get Physical, ${user.sayName()}!</h1>
+  `
+};
 
 function displaySteps() {
   console.log(`${testUsers.allUsersAverageSteps()} average step goal amongst all users!`);
