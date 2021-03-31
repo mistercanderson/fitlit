3c33e373aa272d7bf3d8693f14d2504ec697cbad
Chris Anderson <mistercanderson@gmail.com>
2021-03-30 20:26:44 -0600

    Format user profile card

--- src/scripts.js
+++ src/scripts.js
@@ -8,26 +8,73 @@ const cards = {
   sleep: document.getElementById('sleepHygiene')
 }
 
-function displayUserInfo(user) {
+const profileTab = document.getElementById('profileTab');
+
+window.addEventListener('click', (event) => displayUserInfo(currentUser, event))
+
+function displayUserInfo(user, event) {
   const userKeys = Object.keys(user);
   userKeys.forEach(key => {
     let currentElement = document.getElementById(key)
     switch (key) {
       case 'address':
+        currentElement.innerText = ''
         const address = user[key].split('-')[0];
-        currentElement.innerText += ` ${address}`
+        currentElement.innerText += `${removeCamelCase(key)}: ${address}`
         break
       case 'friends':
+        currentElement.innerText = ''
         const friendNumbers = user[key];
         const userList = userRepo.userList;
-        const friends = friendNumbers.map(friendNumber => 
+        const friends = friendNumbers.map(friendNumber =>
           userList[friendNumber - 1].name);
-        currentElement.innerText += ` ${friends.join(', ')}`
+        currentElement.innerText += `${removeCamelCase(key)}: ${friends.join(', ')}`
         break;
       case 'id':
         break
       default:
-        currentElement.innerText += ` ${user[key]}`
+        currentElement.innerText = ''
+        currentElement.innerText += `${removeCamelCase(key)}: ${user[key]}`
+    };
+  });
+  userProfileToggle(event)
+}
+
+function userProfileToggle(event) {
+  const cardKeys = Object.keys(cards);
+  switch (event.target.id) {
+    case 'profileTab':
+      cardKeys.forEach(cardKey => {
+        if (cardKey === 'user') {
+          cards[cardKey].classList.remove('hidden')
+        } else {
+          cards[cardKey].classList.add('hidden')
+        }
+      })
+      break;
+    case 'closeButton':
+      cardKeys.forEach(cardKey => {
+        if (cardKey === 'user') {
+          cards[cardKey].classList.add('hidden')
+        } else {
+          cards[cardKey].classList.remove('hidden')
+        }
+      })
+      break
+  }
+}
+
+function removeCamelCase(key) {
+  key = Array.from(key);
+  key.forEach((char, index) => {
+    if (key[index].charCodeAt(0) <= 90) {
+      key[index] = ` ${char.toLowerCase()}`;
     }
   })
+  words = key.join('').split(' ');
+  const newPhrase = [];
+  words.forEach(word => {
+    newPhrase.push(word[0].toUpperCase() + word.slice(1));
+  })
+  return newPhrase.join(' ')
 }
