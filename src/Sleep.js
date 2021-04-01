fe545a59006bcb0ec33ed65c6016b83345300e83
Chris Anderson <mistercanderson@gmail.com>
2021-03-31 22:38:58 -0600

    Write Sleep class

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,8 +1,143 @@
 class Sleep {
-  constructor() {
-
+  constructor(userId, sleepData) {
+    this.userId = userId;
+    this.data = sleepData;
+    this.userData = this.extractData();
   }
 
+  extractData() {
+    const userData = this.data.filter(user => user.userID === this.userId);
+    return userData;
+  }
+
+  findDay(date) {
+    const day = this.userData.find(dataPoint => dataPoint.date === date);
+    return day
+  }
+
+  calculateSum(arrayOfNumbers) {
+    return arrayOfNumbers.reduce((a, b) => a + b)
+  }
+
+  calculateAverage(arrayOfNumbers) {
+    const average = this.calculateSum(arrayOfNumbers) / arrayOfNumbers.length;
+    return Number(average.toFixed(1))
+  }
+
+  findDailyHours(date) {
+    const day = this.findDay(date)
+    if (day) {
+      return day.hoursSlept
+    } else {
+      return 'Sorry, you don\'t have any sleep information from that day'
+    }
+  }
+
+  calculateAverageHoursTotal() {
+    const sleepHours = this.userData.map(dataPoint => dataPoint.hoursSlept);
+    return this.calculateAverage(sleepHours)
+  }
+
+  calculateAverageHoursWeekly(endDate) {
+    const day = this.findDay(endDate);
+    if (day) {
+      const userData = this.userData.reverse()
+      const index = userData.indexOf(day);
+      const week = userData.splice(index, 7);
+      const weekHours = week.map(day => day.hoursSlept);
+      return this.calculateAverage(weekHours)
+    } else {
+      return 'Sorry, you don\'t have sleep data from all the days within the range you entered.'
+    }
+  }
+
+  findDailyQuality(date) {
+    const day = this.findDay(date)
+    if (day) {
+      return day.sleepQuality
+    } else {
+      return 'Sorry, you don\'t have any sleep information from that day'
+    }
+  }
+
+  calculateAverageQualityTotal() {
+    const sleepQualities = this.userData.map(dataPoint => dataPoint.sleepQuality);
+    return this.calculateAverage(sleepQualities)
+  }
+
+  calculateAverageQualityWeekly(endDate) {
+    const day = this.findDay(endDate);
+    if (day) {
+      const userData = this.userData.reverse()
+      const index = userData.indexOf(day);
+      const week = userData.splice(index, 7);
+      const weekQualities = week.map(day => day.sleepQuality);
+      return this.calculateAverage(weekQualities)
+    } else {
+      return 'Sorry, you don\'t have sleep data from all the days within the range you entered.'
+    }
+  }
+
+  calculateGlobalQualityAverage() {
+    const globalSleepQualities = this.data.map(dataPoint => dataPoint.sleepQuality);
+    return this.calculateAverage(globalSleepQualities)
+  }
+
+  getWeekDates(endDate) {
+    const week = [];
+    let day = Number(endDate.split('/')[2])
+    for (let i = 0; i < 7; i++) {
+      if (i) {
+        day--
+      }
+      let date = endDate.split('/').splice(0, 2);
+      date.push(String(day))
+      date = date.join('/');
+      week.push(date)
+    }
+    return week
+  }
+
+  getUserIds() {
+    let users = this.data.map(point => point.userID);
+    let userIds = users.sort().filter((user, index, arr) =>
+      !index || user != arr[index - 1]);
+    return userIds
+  }
+
+  findTopSleepersWeekly(date) {
+    const week = this.getWeekDates(date);
+    let weeklyData = [];
+    const topSleepers = [];
+    week.forEach(day => {
+      weeklyData.push(this.data.filter(point => point.date === day));
+    })
+    weeklyData = weeklyData.flat()
+    this.getUserIds().forEach(id => {
+      let userGroup = weeklyData.filter(user => user.userID === id);
+      let userQuality = userGroup.map(day => day.sleepQuality);
+      let userAverage = this.calculateAverage(userQuality);
+      if (userAverage > 3) {
+        topSleepers.push(id)
+      }
+    })
+    return topSleepers;
+  }
+
+  findDeepestSleepers(day) {
+    const dailyData = this.data.filter(point => point.date === day);
+    const userSleepHours = dailyData.map(point => point.hoursSlept);
+    const highestTotal = Math.max(...userSleepHours);
+    const deepestSleepers = []
+    dailyData.forEach(point => {
+      if (point.hoursSlept === highestTotal) {
+        deepestSleepers.push(point.userID)
+      }
+    })
+    return deepestSleepers;
+  }
 }
 
-module.exports = Sleep
+if (typeof module !== 'undefined') {
+  module.exports = Sleep;
+}
--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -14,27 +14,27 @@ describe('Sleep', () => {
     sleep = new Sleep(user1.id, sleepData)
   });
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     expect(Sleep).to.be.a('function');
   });
 
-  it.skip('should be an instance of Sleep', () => {
+  it('should be an instance of Sleep', () => {
     expect(sleep).to.be.an.instanceof(Sleep);
   });
 
-  it.skip('should store sleep data and a user ID', () => {
+  it('should store sleep data and a user ID', () => {
     expect(sleep.data).to.deep.equal(sleepData);
     expect(sleep.userId).to.equal(user1.id);
   });
 
-  it.skip('should be able to extract a user\'s sleep data from the rest of the sleep data', () => {
+  it('should be able to extract a user\'s sleep data from the rest of the sleep data', () => {
     const extractedData = sleep.extractData();
 
     expect(sleep.userData).to.be.an('Array');
     expect(extractedData).to.deep.equal(sleep.userData);
   })
 
-  it.skip('should find the number of hours a user slept on a certain day', () => {
+  it('should find the number of hours a user slept on a certain day', () => {
     const hoursSlept = sleep.findDailyHours("2019/06/15");
     const wrongHoursSlept = sleep.findDailyHours("1976/03/31");
 
@@ -42,24 +42,23 @@ describe('Sleep', () => {
     expect(wrongHoursSlept).to.equal('Sorry, you don\'t have any sleep information from that day')
   });
 
-  it.skip('should calculate the average hours a user has slept over all time', () => {
-    const totalHoursAverage = sleep.calculateAverageHoursTotal(sleep.userId);
+  it('should calculate the average hours a user has slept over all time', () => {
+    const totalHoursAverage = sleep.calculateAverageHoursTotal();
 
-    expect(totalHoursAverage).to.equal(6.6)
+    expect(totalHoursAverage).to.equal(6.7)
   });
 
-  it.skip('should calculate the average hours a user slept over the course of a week', () => {
-    const weeklyHoursAverage = sleep.calculateAverageHoursWeekly("2019/06/15", "2019/06/21");
+  it('should calculate the average hours a user slept over the course of a week', () => {
+    const weeklyHoursAverage = sleep.calculateAverageHoursWeekly("2019/06/21");
 
     expect(weeklyHoursAverage).to.equal('Sorry, you don\'t have sleep data from all the days within the range you entered.');
     const sleep2 = new Sleep(2, sleepData);
-    sleep2.extractData();
-    const weeklyHoursAverage2 = sleep2.calculateAverageHoursWeekly("2019/06/15", "2019/06/21");
+    const weeklyHoursAverage2 = sleep2.calculateAverageHoursWeekly("2019/06/21");
 
     expect(weeklyHoursAverage2).to.equal(10);
   });
 
-  it.skip('should find the sleep quality a user had on a certain day', () => {
+  it('should find the sleep quality a user had on a certain day', () => {
     const sleepQuality = sleep.findDailyQuality("2019/06/15");
     const wrongSleepQuality = sleep.findDailyQuality("1976/03/31");
 
@@ -67,41 +66,36 @@ describe('Sleep', () => {
     expect(wrongSleepQuality).to.equal('Sorry, you don\'t have any sleep information from that day')
   });
 
-  it.skip('should calculate the average sleep quality a user has had over all time', () => {
+  it('should calculate the average sleep quality a user has had over all time', () => {
     const totalQualityAverage = sleep.calculateAverageQualityTotal(sleep.userId);
 
-    expect(totalQualityAverage).to.equal(2.6)
+    expect(totalQualityAverage).to.equal(2.7)
   });
 
-  it.skip('should calculate the average sleep quality a user had over the course of a week', () => {
-    const weeklyQualityAverage = sleep.calculateAverageQualityWeekly("2019/06/15", "2019/06/21");
+  it('should calculate the average sleep quality a user had over the course of a week', () => {
+    const weeklyQualityAverage = sleep.calculateAverageQualityWeekly("2019/06/21");
 
     expect(weeklyQualityAverage).to.equal('Sorry, you don\'t have sleep data from all the days within the range you entered.');
     const sleep2 = new Sleep(2, sleepData);
-    sleep2.extractData();
-    const weeklyQualityAverage2 = sleep2.calculateAverageQualityWeekly("2019/06/15", "2019/06/21");
+    const weeklyQualityAverage2 = sleep2.calculateAverageQualityWeekly("2019/06/21");
 
     expect(weeklyQualityAverage2).to.equal(1);
   });
 
-  it.skip('should be able to calculate the average sleep quality for all users', () => {
+  it('should be able to calculate the average sleep quality for all users', () => {
     const globalAverage = sleep.calculateGlobalQualityAverage();
 
-    expect(globalAverage).to.equal(1.5)
+    expect(globalAverage).to.equal(2.5)
   });
 
-  it.skip('should be able to find the id of all users with an average sleep quality over 3 in any given week', () => {
-    const topSleepers = sleep.findTopSleepersWeekly("2019/06/15", "2019/06/21");
+  it('should be able to find the id of all users with an average sleep quality over 3 in any given week', () => {
+    const topSleepers = sleep.findTopSleepersWeekly("2019/06/21");
     
     expect(topSleepers).to.be.an('Array');
     expect(topSleepers).to.deep.equal([3]);
-
-    const wrongTop = sleep.findTopSleepersWeekly("2019/06/15", "2019/06/30");
-
-    expect(wrongTop).to.equal('Please enter a range of 7 days')
   });
 
-  it.skip('should find the sleepiest user(s) on any given date', () => {
+  it('should find the sleepiest user(s) on any given date', () => {
     const sleepyGuy1 = sleep.findDeepestSleepers("2019/06/15");
 
     expect(sleepyGuy1).to.deep.equal([3]);
